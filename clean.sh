0828e662b726e86428a172ecd4791d55c303b772
Matteo Conti <matteo.conti.977@gmail.com>
2024-07-25 18:46:18 +0200

    update clean script

--- clean.sh
+++ clean.sh
@@ -14,4 +14,18 @@ cd reference-monitor || { echo "Failed to enter directory reference-monitor"; ex
 # Run make
 make clean || { echo "Make command failed"; exit 1; }
 
+cd ..
+
+# Navigate to directory user
+cd user || { echo "Failed to enter directory user"; exit 1; }
+
+rm a.out
+
+cd ..
+
+# Navigate to directory utils
+cd utils || { echo "Failed to enter directory utils"; exit 1; }
+rm utils.o
+rm .utils.o.cmd
+
 echo "Cleaned successfully"

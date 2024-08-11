16350d9189c669843044d1b1f8f763a65bae3297
Matteo Conti <matteo.conti.977@gmail.com>
2024-08-11 17:30:24 +0200

    readme

--- test/insert_resource.c
+++ test/insert_resource.c
@@ -35,21 +35,19 @@ int main(int argc, char *argv[]){
     char *passwd;
     char *path;
 
-    if(argc < 4){
-        printf("Usage: %s <num_copies> <path> <password>\n", argv[0]);
+    if(argc < 3){
+        printf("Usage: %s <path> <password>\n", argv[0]);
         return -1;
     }
 
-    int num_copies = atoi(argv[1]);
-    path = malloc(strlen(argv[2]) + 1);
-    strcpy(path, argv[2]);
-    passwd = malloc(strlen(argv[3]) + 1);
-    strcpy(passwd, argv[3]);
+    path = malloc(strlen(argv[1]) + 1);
+    strcpy(path, argv[1]);
+    passwd = malloc(strlen(argv[2]) + 1);
+    strcpy(passwd, argv[2]);
 
-    printf("Adding resource %s %d times\n", path, num_copies);
-
-    for(int i = 0; i < num_copies; i++)
-        check_add(path, passwd);
+    printf("Adding resource %s\n", path);
+    
+    check_add(path, passwd);
 
 	return 0;
 }
